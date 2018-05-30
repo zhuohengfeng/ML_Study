d762641c5b1b2303a70aa8ef67bae02f7184c0fb
zhuohf1 <zhuohf1@lenovo.com>
2018-05-30 09:04:31 +0800

    Initial commit

---
+++ 01_kNN.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/30 8:57
+# @Author  : zhuo_hf@foxmail.com
+# @Site    : 
+# @File    : 01_kNN.py
+# @Software: PyCharm
+
+import numpy as np
+from math import sqrt
+from collections import Counter
+
+
+def kNN_classify(k, X_train, Y_train, x):
+    pass
